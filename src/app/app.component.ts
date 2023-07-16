9fc19ae158daaa1e09b4625d66ba1f7e89ef377c
Santiago Gonzalez <sagonzalezbu@gmail.com>
2023-07-16 16:44:25 -0500

    Docs creado

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+
+})
+export class AppComponent {
+
+  public title: string = 'Mi primera app de Angular';
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    DbzModule,
+    HeroesModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
